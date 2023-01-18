5ebf150561086f6429100784c3f50e16102a23a9
Yaroslav-Pa <yaroslav.roro@gmail.com>
2023-01-18 22:14:20 +0200

    working properli by sity

--- d5/weather-project/src/components/MainWindow.vue
+++ d5/weather-project/src/components/MainWindow.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      Cityname: "Zaporizhia",
+      Cityname: "London",
 
       chosenCitys: [],
       infoAboutCity: [],
@@ -17,10 +17,8 @@ export default {
       .then((response) => {
         console.log(response.data);
         this.infoAboutCity = response.data;
-        localStorage.setItem("infoAboutCityArr", this.infoAboutCity);
         console.log(this.infoAboutCity.main.pressure);
       });
-    this.infoAboutCity = localStorage.getItem("infoAboutCityArr");
   },
   methods: {},
 };
@@ -35,56 +33,78 @@ export default {
       <option v-for="city in chosenCitys">{{ city }}</option>
     </select>
 
-    <p>For {{ this.Cityname }}</p>
-    <div class="">
-      <table class="tableForDisplay">
-        <tr>
-          <th>{{ this.Cityname }}</th>
-          <th><img src="https://img.icons8.com/office/16/null/blur.png" /></th>
-          <th>
+    <div class="block">
+      <!-- <h1>For {{ this.Cityname }}</h1> -->
+      <div class="tableForDisplay">
+        <div class="childTable">
+          <p>{{ this.Cityname }}</p>
+          <p><img src="https://img.icons8.com/office/16/null/blur.png" /></p>
+          <p>
             <img
               src="https://img.icons8.com/external-kosonicon-flat-kosonicon/64/null/external-temperatures-temperature-kosonicon-flat-kosonicon.png"
             />
-          </th>
-          <th>
+          </p>
+          <p>
             <img
               src="https://img.icons8.com/external-flat-amoghdesign/32/null/external-cloud-weather-vol-01-flat-amoghdesign-18.png"
             />
-          </th>
-          <th>
+          </p>
+          <p>
             <img src="https://img.icons8.com/officel/16/null/pressure.png" />
-          </th>
-          <th>
+          </p>
+          <p>
             <img
               src="https://img.icons8.com/external-microdots-premium-microdot-graphic/64/null/external-weather-weather-forecast-microdots-premium-microdot-graphic-4.png"
             />
-          </th>
-        </tr>
-        <tr>
-          <td>{{ this.infoAboutCity.sys.country }}</td>
-          <td>humidity</td>
-          <td>temp</td>
-          <td>main</td>
-          <td>pressure</td>
-          <td>description</td>
-        </tr>
-        <tr>
-          <td>
-            {{ this.infoAboutCity.coord.lat }};
-            {{ this.infoAboutCity.coord.lon }}
-          </td>
-          <td>{{ this.infoAboutCity.main.humidity }}</td>
-          <td>{{ this.infoAboutCity.main.temp }}</td>
-          <td>{{ this.infoAboutCity.weather[0].main }}</td>
-          <td>{{ this.infoAboutCity.main.pressure }}</td>
-          <td>{{ this.infoAboutCity.weather[0].description }}</td>
-        </tr>
-      </table>
+          </p>
+        </div>
+        <div class="childTable">
+          <p>{{ this.infoAboutCity.sys.country }}</p>
+          <p>humidity</p>
+          <p>temp</p>
+          <p>main</p>
+          <p>pressure</p>
+          <p>description</p>
+        </div>
+        <div class="childTable">
+          <p>
+            [{{ this.infoAboutCity.coord.lat }};
+            {{ this.infoAboutCity.coord.lon }}]
+          </p>
+          <p>{{ this.infoAboutCity.main.humidity }}</p>
+          <p>{{ this.infoAboutCity.main.temp }}</p>
+          <p>{{ this.infoAboutCity.weather[0].main }}</p>
+          <p>{{ this.infoAboutCity.main.pressure }}</p>
+          <p>{{ this.infoAboutCity.weather[0].description }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
 .tableForDisplay {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 25%;
+  background: rgb(51, 51, 51, 0.7);
+  border-radius: 30px;
+}
+.tableForDisplay img {
+  width: 20px;
+}
+.childTable p {
+  display: flex;
+
+  /* justify-content: center; */
+  /* padding: 50px; */
+}
+.childTable {
+  margin: 30px;
+}
+.block {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
